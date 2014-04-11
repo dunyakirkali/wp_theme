255d76bb9dd642dc63ea152ce78c151201c8e637
Dunya Kirkali <dunyakirkali@gmail.com>
2014-04-11 15:03:46 +0200

    improved integration

--- header.php
+++ header.php
@@ -3,5 +3,7 @@
   <title>Ilana's Dream'</title>
   
   <link href='<?php bloginfo('stylesheet_url'); ?>' rel='stylesheet' type='text/css' media='screen'>
+  
+  <link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
 </head>
 <body>
--- index.php
+++ index.php
@@ -3,26 +3,28 @@
 <div id='container'>
   <div id='left'>
     <div id='caption'>
-      <p>MATHILDE</p>
+      <p><?php echo get_bloginfo('name'); ?></p>
       <p>Atelier de couture</p>
     </div>
   </div>
   
   <div id='right'>
-    <?php wp_nav_menu( array( 'theme_location' => 'top-menu' ) ); ?>
+    <div id='right-container'>
+      <?php wp_nav_menu( array( 'theme_location' => 'top-menu' ) ); ?>
     
-    <div>
-      <?php while(have_posts()) : the_post(); ?>
-        <div class='page'>
-          <h2><?php the_title(); ?></h2>
-          <?php the_content(); ?>
-        </div>
-      <?php endwhile; ?>
-    </div>
+      <div>
+        <?php while(have_posts()) : the_post(); ?>
+          <div class='page'>
+            <h2><?php the_title(); ?></h2>
+            <?php the_content(); ?>
+          </div>
+        <?php endwhile; ?>
+      </div>
     
-    <div id='copyright'>
-      <p>© 2014 Mathilde, All Rights Reserved. Back to top</p>
-    </div>
+      <div id='copyright'>
+        <p>© 2014 Mathilde, All Rights Reserved. Back to top</p>
+      </div>
+    </div>      
   </div>
 </div>
 
