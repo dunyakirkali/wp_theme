9ad5ed2cc64a96950ff0b8acaa0e68d824bbe32a
Dunya Kirkali <dunyakirkali@gmail.com>
2014-04-11 14:21:24 +0200

    base

--- header.php
+++ header.php
@@ -3,7 +3,5 @@
   <title>Ilana's Dream'</title>
   
   <link href='<?php bloginfo('stylesheet_url'); ?>' rel='stylesheet' type='text/css' media='screen'>
-  
-  <?php wp_head(); ?>
 </head>
 <body>
--- index.php
+++ index.php
@@ -11,6 +11,15 @@
   <div id='right'>
     <?php wp_nav_menu( array( 'theme_location' => 'top-menu' ) ); ?>
     
+    <div>
+      <?php while(have_posts()) : the_post(); ?>
+        <div class='page'>
+          <h2><?php the_title(); ?></h2>
+          <?php the_content(); ?>
+        </div>
+      <?php endwhile; ?>
+    </div>
+    
     <div id='copyright'>
       <p>© 2014 Mathilde, All Rights Reserved. Back to top</p>
     </div>
