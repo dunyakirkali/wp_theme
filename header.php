a0c2f1d68e7c40a0f27369eee1c9e2133cb70209
Dunya Kirkali <dunyakirkali@gmail.com>
2014-04-12 20:07:59 +0200

    foundation power

--- header.php
+++ header.php
@@ -1,9 +1,22 @@
-<html>
-<head>
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
   <title>Ilana's Dream'</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   
-  <link href='<?php bloginfo('stylesheet_url'); ?>' rel='stylesheet' type='text/css' media='screen'>
+  <link rel="stylesheet" type="text/css" href='<?php bloginfo('stylesheet_directory'); ?>/foundation.css' />  
+  <link href='<?php bloginfo('stylesheet_url'); ?>' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" media="only screen and (max-device-width: 480px)" href='<?php bloginfo('stylesheet_directory'); ?>/mobile.css' />
   
   <link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
+  
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="<?php bloginfo('stylesheet_directory'); ?>/js/vendor/modernizr.js"></script>  
+  <script src="<?php bloginfo('stylesheet_directory'); ?>/js/foundation.min.js"></script>  
+  <script src="<?php bloginfo('stylesheet_directory'); ?>/js/foundation/foundation.dropdown.js"></script>
+  <script src="<?php bloginfo('stylesheet_directory'); ?>/js/foundation/foundation.equalizer.js"></script>  
+  <script src="<?php bloginfo('stylesheet_directory'); ?>/theme.js"></script>
 </head>
+<a name="top"></a>
 <body>
--- index.php
+++ index.php
@@ -1,30 +1,66 @@
-<?php get_header(); ?>
-
-<div id='container'>
-  <div id='left'>
-    <div id='image'>
-      <?php the_post_thumbnail(); ?>
+<?php 
+  get_header();
+?>
+<?php 
+  if ( has_post_thumbnail() ) {
+    $post_image_id = get_post_thumbnail_id($post_to_use->ID);
+    if ($post_image_id) {
+			$thumbnail = wp_get_attachment_image_src( $post_image_id, 'post-thumbnail', false);
+			if ($thumbnail) (string)$thumbnail = $thumbnail[0];
+		}
+  }
+?>
+    
+<div class='row'>
+  <div id='first' class='columns small-12 medium-6 large-6' style="background: url('<?php echo $thumbnail; ?>') no-repeat center center fixed;">
+    <div class='row' id="placeholder_1"></div>
+    <div class='row'>
+      <div class='columns small-10 medium-10 large-10 small-push-1 medium-push-1 large-push-1' id='caption'>
+        <div class='row'>
+          <p id='site-title'><?php echo get_bloginfo('name'); ?></p>
+        </div>
+        <div class='row'>
+          <p><?php echo get_bloginfo('description'); ?></p>
+        </div>
+      </div>
     </div>
-    <div id='caption'>
-      <p><?php echo get_bloginfo('name'); ?><?php echo get_bloginfo('description'); ?></p>
+    <div class='row' id="placeholder_2"></div>    
+    <div class='row show-for-small'>    
+      <div id='mobile-menu' class='columns small-10 medium-10 large-10 small-push-1 medium-push-1 large-push-1'>
+        <a href="#" data-dropdown="menu-menu"><?php the_title(); ?></a>
+        <?php wp_nav_menu('menu=top-menu'); ?>
+      </div>
     </div>
+    <div class='row' id="placeholder_3"></div>    
   </div>
   
-  <div id='right'>
-    <div id='right-container'>
-      <?php wp_nav_menu('menu=top-menu&after=<span>.</span>'); ?>
-    
-      <?php while(have_posts()) : the_post(); ?>
-        <div class='page'>
-          <h2><?php the_title(); ?></h2>
-          <?php the_content(); ?>
-        </div>
-      <?php endwhile; ?>
-      <div id='copyright'>
-        <p>© 2014 Mathilde, All Rights Reserved. Back to top</p>
+  <div id='second' class='columns small-12 medium-6 large-6'>
+    <div class='row hide-for-small'>    
+      <div class='columns small-12 medium-12 large-12'>
+        <?php wp_nav_menu('menu=top-menu&after=<span>.</span>'); ?>
       </div>
-    </div>      
-  </div>
+    </div>
+    <div class='row'>    
+      <div class='columns small-10 medium-10 large-10 small-push-1 medium-push-1 large-push-1'>
+        <?php while(have_posts()) : the_post(); ?>
+          <div class='page'>
+            <h2><?php the_title(); ?></h2>
+            <?php the_content(); ?>
+          </div>
+        <?php endwhile; ?>
+      </div>
+    </div>
+    
+    <div class='row'>    
+      <div class='columns small-10 medium-10 large-10 small-push-1 medium-push-1 large-push-1'>
+        <p>© 2014 Mathilde, All Rights Reserved.</p>
+        <a href='#top'>Back to top</a>
+      </div>
+    </div>
+  </div>  
 </div>
+
+
+
 
 <?php get_footer(); ?>
